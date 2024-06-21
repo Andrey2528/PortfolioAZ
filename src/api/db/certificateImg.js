caf5c3281f26023fbd61830cb0b1002ae98c0994
Andrey2528 <andreys20.2016@ukr.net>
2024-06-21 22:30:25 +0300

    fetch: add new certificate

--- src/api/db/certificateImg.js
+++ src/api/db/certificateImg.js
@@ -3,8 +3,36 @@ import certificateBlockchain2 from '@/assets/images/sertificateIMG/certificateBl
 import certificateBlockchain3 from '@/assets/images/sertificateIMG/certificateBlockchain3.png';
 import certificateBlockchain4 from '@/assets/images/sertificateIMG/certificateBlockchain4.png';
 import certificateMarketing from '@/assets/images/sertificateIMG/certificateMarketing.png';
+import certificateWeb1 from '@/assets/images/sertificateIMG/certificateWeb1.png';
+import certificateWeb2 from '@/assets/images/sertificateIMG/certificateWeb2.png';
+import certificateWeb3 from '@/assets/images/sertificateIMG/certificateWeb3.png';
+import certificateWeb4 from '@/assets/images/sertificateIMG/certificateWeb4.png';
 
 export const certificateImg = [
+    {
+        id: 8,
+        title: 'certificateWeb4',
+        date: '2023-05-26',
+        img: certificateWeb4,
+    },
+    {
+        id: 7,
+        title: 'certificateWeb3',
+        date: '2022-03-20',
+        img: certificateWeb3,
+    },
+    {
+        id: 6,
+        title: 'certificateWeb2',
+        date: '2022-03-19',
+        img: certificateWeb2,
+    },
+    {
+        id: 5,
+        title: 'certificateWeb1',
+        date: '2022-01-11',
+        img: certificateWeb1,
+    },
     {
         id: 4,
         title: 'certificateBlockchain4',
