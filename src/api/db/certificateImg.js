47d415ab334d3ac8c64c523401f93fd75bc8facf
Andrey2528 <andreys20.2016@ukr.net>
2024-06-22 22:23:23 +0300

    fix: add transle for slide img title

--- src/api/db/certificateImg.js
+++ src/api/db/certificateImg.js
@@ -1,3 +1,4 @@
+import i18next from '@/i18n';
 import certificateBlockchain1 from '@/assets/images/sertificateIMG/certificateBlockchain1.png';
 import certificateBlockchain2 from '@/assets/images/sertificateIMG/certificateBlockchain2.png';
 import certificateBlockchain3 from '@/assets/images/sertificateIMG/certificateBlockchain3.png';
@@ -11,55 +12,55 @@ import certificateWeb4 from '@/assets/images/sertificateIMG/certificateWeb4.png'
 export const certificateImg = [
     {
         id: 8,
-        title: 'certificateWeb4',
+        title: i18next.t('certificate.title9'),
         date: '2023-05-26',
         img: certificateWeb4,
     },
     {
         id: 7,
-        title: 'certificateWeb3',
+        title: i18next.t('certificate.title8'),
         date: '2022-03-20',
         img: certificateWeb3,
     },
     {
         id: 6,
-        title: 'certificateWeb2',
+        title: i18next.t('certificate.title7'),
         date: '2022-03-19',
         img: certificateWeb2,
     },
     {
         id: 5,
-        title: 'certificateWeb1',
+        title: i18next.t('certificate.title6'),
         date: '2022-01-11',
         img: certificateWeb1,
     },
     {
         id: 4,
-        title: 'certificateBlockchain4',
+        title: i18next.t('certificate.title5'),
         date: '2024-06-12',
         img: certificateBlockchain4,
     },
     {
         id: 3,
-        title: 'certificateBlockchain3',
+        title: i18next.t('certificate.title4'),
         date: '2024-06-12',
         img: certificateBlockchain3,
     },
     {
         id: 2,
-        title: 'certificateBlockchain2',
+        title: i18next.t('certificate.title3'),
         date: '2024-06-12',
         img: certificateBlockchain2,
     },
     {
         id: 1,
-        title: 'certificateBlockchain1',
+        title: i18next.t('certificate.title2'),
         date: '2024-06-12',
         img: certificateBlockchain1,
     },
     {
         id: 0,
-        title: 'certificateMarketing',
+        title: i18next.t('certificate.title1'),
         date: '2024-06-20',
         img: certificateMarketing,
     },
