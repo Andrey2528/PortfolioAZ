ed26608972764b354d42fd04fc0180bbb9ba6032
Andrey2528 <andreys20.2016@ukr.net>
2025-03-23 16:12:34 +0200

    Add router to main pahe

--- src/api/db/navPage.js
+++ src/api/db/navPage.js
@@ -3,11 +3,16 @@ import i18next from '@/i18n';
 export const navPage = [
     {
         id: 1,
+        title: i18next.t('navMenu.link7'),
+        route: '/',
+    },
+    {
+        id: 2,
         title: i18next.t('navMenu.link2'),
         route: '/InfoPage',
     },
     {
-        id: 2,
+        id: 3,
         title: i18next.t('navMenu.link3'),
         route: '/SertificatePage',
     },
