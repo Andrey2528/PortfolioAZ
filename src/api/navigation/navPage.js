6d09243cba68b9f5ec70b6c9cda4c2f863357c12
Andrey2528 <andreys20.2016@ukr.net>
2025-04-09 02:23:15 +0300

    fix route links

--- src/api/navigation/navPage.js
+++ src/api/navigation/navPage.js
@@ -3,17 +3,17 @@ import i18next from '@/i18n';
 export const navPage = [
     {
         id: 1,
-        title: i18next.t('navMenu.link7'),
-        route: '/',
-    },
-    {
-        id: 2,
         title: i18next.t('navMenu.link2'),
         route: '/InfoPage',
     },
     {
-        id: 3,
+        id: 2,
         title: i18next.t('navMenu.link3'),
+        route: '/',
+    },
+    {
+        id: 3,
+        title: i18next.t('navMenu.link4'),
         route: '/SertificatePage',
     },
 ];
